519544d18f93386c61f8043273fcf9c116397a9b
sergio <sbpss@iscte-iul.pt>
2017-11-29 22:03:16 +0000

    Foi criado ao package analise de emails com as classes Emails_processing e Main_Analise_de_Emails. a primeira contem os metodos para avaliar quantidade de falsos positivos e falsos negativos. a segunda serve para testes

---
+++ src/Analise_de_Emails/Main_Analise_de_Emails.java
@@ -0,0 +1,8 @@
+package Analise_de_Emails;
+
+public class Main_Analise_de_Emails {
+	public static void main(String[] args) {
+		Emails_Processing tmp = new Emails_Processing("", "", "");
+		tmp.avaliar();
+	}
+}
